305421220c8ec75b22816164c1c606bb28a499b1
Yao Yue <yao@twitter.com>
2014-06-22 17:46:40 -0700

    check in for sync

---
+++ src/bb_memcache.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+
+#include <cc_mbuf.h>
+
+#include <bb_memcache.h>
+
+void
+parse_reqhdr(struct mq *msg)
+{
+
+}
---
+++ src/bb_memcache.h
@@ -0,0 +1,11 @@
+#ifndef __BB_MEMCACHE_H__
+#define __BB_MEMCACHE_H__
+
+#include <cc_mbuf.h>
+
+#define MAX_KEY_LEN 250
+
+
+void parse_reqhdr(struct mq *msg);
+
+#endif
