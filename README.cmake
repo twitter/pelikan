1ba6373cb2f8bc7d1b59575efaa0805b7f36eecd
Yao Yue <yao@twitter.com>
2014-11-21 16:11:44 -0800

    left config.h.in

--- README.cmake
+++ README.cmake
@@ -1,3 +1,6 @@
+# checkout ccommon source, it should be a sibling directory of broadbill
+# otherwise the path needs to be explicitly set with `CCOMMON_SOURCE_DIR`
+
 mkdir _build
 cd _build
 cmake ..
