e4caab64e9c0e4afd10c5cce949e76c044d55489
Yao Yue <yao@twitter.com>
2014-11-21 17:19:39 -0800

    minor update to cmake and readme

--- README.cmake
+++ README.cmake
@@ -1,6 +1,10 @@
 # checkout ccommon source, it should be a sibling directory of broadbill
 # otherwise the path needs to be explicitly set with `CCOMMON_SOURCE_DIR`
 
+# to turn on/off various compile-time switches, use -D option with cmake
+# Example:
+#     cmake -DHAVE_LOGGING=OFF
+
 mkdir _build
 cd _build
 cmake ..
