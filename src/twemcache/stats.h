5992a26c4ca564cd4f02d3a909a61c8127280725
Kevin Yang <kyang@twitter.com>
2015-11-20 18:54:01 -0800

    alphabetize stats

--- src/twemcache/stats.h
+++ src/twemcache/stats.h
@@ -12,21 +12,21 @@
 #include <channel/cc_tcp.h>
 
 struct glob_stats {
-    procinfo_metrics_st     procinfo_metrics;
-    event_metrics_st        event_metrics;
-    server_metrics_st       server_metrics;
-    worker_metrics_st       worker_metrics;
     buf_metrics_st          buf_metrics;
-    tcp_metrics_st          tcp_metrics;
-    request_metrics_st      request_metrics;
-    response_metrics_st     response_metrics;
-    parse_req_metrics_st    parse_req_metrics;
     compose_rsp_metrics_st  compose_rsp_metrics;
-    process_metrics_st      process_metrics;
-    slab_metrics_st         slab_metrics;
+    event_metrics_st        event_metrics;
     item_metrics_st         item_metrics;
     log_metrics_st          log_metrics;
     klog_metrics_st         klog_metrics;
+    parse_req_metrics_st    parse_req_metrics;
+    process_metrics_st      process_metrics;
+    procinfo_metrics_st     procinfo_metrics;
+    request_metrics_st      request_metrics;
+    response_metrics_st     response_metrics;
+    server_metrics_st       server_metrics;
+    slab_metrics_st         slab_metrics;
+    tcp_metrics_st          tcp_metrics;
+    worker_metrics_st       worker_metrics;
 };
 
 extern struct glob_stats glob_stats;
