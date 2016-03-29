e160af130241aa5323b8e38ffe173d6efb44dca1
Yao Yue <yao@twitter.com>
2016-03-29 15:15:56 -0700

    Squashed 'deps/ccommon/' changes from 3cdf961..3cb2d10
    3cb2d10 Merge pull request #112 from thinkingfish/revert_tcp_peer
    e18bfa4 Revert "Merge pull request #107 from kevyang/kevyang/tcp_peer_resolution"
    b424baf Revert "Merge pull request #110 from kevyang/kevyang/tcp_test_shadow_declaration"
    d8de1e2 Merge pull request #111 from thinkingfish/event_refactor
    161cd9a add event_del to test; fix metric bug
    ce5c6ac refactor epoll implementation, remove del_read/write since there's no immediate use cases for them
    2bc08b7 update event interface & kqueue implementation
    2185d28 Merge pull request #110 from kevyang/kevyang/tcp_test_shadow_declaration
    32fd418 fix shadow declaration in tcp unit test
    c7a34c3 Merge pull request #107 from kevyang/kevyang/tcp_peer_resolution
    406178b fix in gnu/linux
    c94b66b address comments, add test
    1c19596 add tcp peer resolution
    
    git-subtree-dir: deps/ccommon
    git-subtree-split: 3cb2d10465b0d19e1a1788fc38425493965205c1

--- deps/ccommon/src/event/cc_epoll.c
+++ deps/ccommon/src/event/cc_epoll.c
@@ -117,35 +117,36 @@ event_base_destroy(struct event_base **evb)
     *evb = NULL;
 }
 
+static int
+_event_update(struct event_base *evb, int fd, int op, uint32_t events, void *ptr)
+{
+    struct epoll_event event;
+
+    ASSERT(evb != NULL && evb->ep > 0);
+    ASSERT(fd > 0);
+
+    event.events = events;
+    event.data.ptr = ptr;
+
+    return epoll_ctl(evb->ep, op, fd, &event);
+}
+
 int event_add_read(struct event_base *evb, int fd, void *data)
 {
     int status;
-    struct epoll_event event;
-    int ep;
-
-    ASSERT(evb != NULL);
-
-    ep = evb->ep;
-
-    ASSERT(ep > 0);
-    ASSERT (fd > 0);
-
-    event.events = EPOLLIN;
-    //event.events = (EPOLLIN | EPOLLET);
-    event.data.ptr = data;
 
     /*
      * Note(yao): there have been tests showing EPOLL_CTL_ADD is cheaper than
      * EPOLL_CTL_MOD, and the only difference is we need to ignore EEXIST
      */
-    status = epoll_ctl(ep, EPOLL_CTL_ADD, fd, &event);
+    status = _event_update(evb, fd, EPOLL_CTL_ADD, EPOLLIN, data);
     if (status < 0 && errno != EEXIST) {
-        log_error("ctl (add read) w/ epoll fd %d on fd %d failed: %s", ep, fd,
-                strerror(errno));
+        log_error("ctl (add read) w/ epoll fd %d on fd %d failed: %s", evb->ep,
+                fd, strerror(errno));
     }
 
     INCR(event_metrics, event_read);
-    log_verb("add read event to epoll fd %d on fd %d", ep, fd);
+    log_verb("add read event to epoll fd %d on fd %d", evb->ep, fd);
 
     return status;
 }
@@ -154,84 +155,32 @@ int
 event_add_write(struct event_base *evb, int fd, void *data)
 {
     int status;
-    struct epoll_event event;
-    int ep;
 
-    ASSERT(evb != NULL);
-
-    ep = evb->ep;
-
-    ASSERT(ep > 0);
-    ASSERT(fd > 0);
-
-    event.events = EPOLLOUT;
-    //event.events = (EPOLLOUT | EPOLLET);
-    event.data.ptr = data;
-
-    /* Note(yao): see note in event_add_read about epoll_ctl() */
-    status = epoll_ctl(ep, EPOLL_CTL_ADD, fd, &event);
+    status = _event_update(evb, fd, EPOLL_CTL_ADD, EPOLLOUT, data);
     if (status < 0 && errno != EEXIST) {
-        log_error("ctl (add write) w/ epoll fd %d on fd %d failed: %s", ep, fd,
-                status, strerror(errno));
+        log_error("ctl (add write) w/ epoll fd %d on fd %d failed: %s", evb->ep,
+                 fd, strerror(errno));
     }
 
     INCR(event_metrics, event_write);
-    log_verb("add write event to epoll fd %d on fd %d", ep, fd);
+    log_verb("add write event to epoll fd %d on fd %d", evb->ep, fd);
 
     return status;
 }
 
 int
-event_register(struct event_base *evb, int fd, void *data)
+event_del(struct event_base *evb, int fd)
 {
     int status;
-    struct epoll_event event;
-    int ep;
-
-    ASSERT(evb != NULL);
-
-    ep = evb->ep;
-
-    ASSERT(ep > 0);
-    ASSERT(fd > 0);
-
-    event.events = (EPOLLOUT | EPOLLIN);
-    //event.events = (EPOLLOUT | EPOLLIN | EPOLLET);
-    event.data.ptr = data;
-
-    status = epoll_ctl(ep, EPOLL_CTL_ADD, fd, &event);
-    if (status < 0) {
-        log_error("ctl (reg) of fd %d to epoll fd %d failed: %s", fd, ep,
-                strerror(errno));
-    }
-
-    log_verb("register fd %d to epoll fd %d", fd, ep);
-
-    return status;
-}
-
-int
-event_deregister(struct event_base *evb, int fd)
-{
-    int status;
-    struct epoll_event event;
-    int ep;
-
-    ASSERT(evb != NULL);
-
-    ep = evb->ep;
-
-    ASSERT(ep > 0);
-    ASSERT(fd > 0);
 
     /* event can be NULL in kernel >=2.6.9, here we keep it for compatibility */
-    status = epoll_ctl(ep, EPOLL_CTL_DEL, fd, &event);
+    status = _event_update(evb, fd, EPOLL_CTL_DEL, 0, NULL);
     if (status < 0) {
-        log_error("ctl (dereg) of fd %d from epoll fd %d failed: %s", fd, ep,
+        log_error("ctl (del) w/ epoll fd %d on fd %d failed: %s", evb->ep, fd,
                 strerror(errno));
     }
 
-    log_verb("deregister fd %d from epoll fd %d", fd, ep);
+    log_verb("del fd %d from epoll fd %d", fd, evb->ep);
 
     return status;
 }
