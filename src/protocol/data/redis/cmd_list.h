d7c18ef535469daf3373cb209b4a417ea10c4076
Yao Yue <yao@twitter.com>
2018-05-15 19:20:15 -0700

    adding list interface

---
+++ src/protocol/data/redis/cmd_list.h
@@ -0,0 +1,44 @@
+#pragma once
+
+/**
+ * create: create an empty list
+ * List.create KEY
+ *
+ * delete: delete a list or a particular value from list
+ * List.delete KEY [VALUE [COUNT]]
+ *
+ * delete: trimming a list
+ * List.trim KEY START STOP
+ *
+ * len: return number of entries in list
+ * List.len KEY
+ *
+ * find: find entry in list
+ * List.find KEY VALUE
+ *
+ * get: get entry of an index
+ * List.get KEY [START [STOP]]
+ *
+ * insert: insert entry at an index
+ * List.insert KEY VALUE INDEX
+ *
+ * push: pushing entry/entries at the end
+ * List.push KEY VALUE [VALUE ...]
+ */
+
+
+/*          type                string          #arg    #opt */
+#define REQ_LIST(ACTION)                                      \
+    ACTION( REQ_LIST_CREATE,  "List.create",    2,      0  )\
+    ACTION( REQ_LIST_DELETE,  "List.delete",    2,      2  )\
+    ACTION( REQ_LIST_TRIM,    "List.trim",      4,      0  )\
+    ACTION( REQ_LIST_LEN,     "List.len",       2,      0  )\
+    ACTION( REQ_LIST_FIND,    "List.find",      3,      0  )\
+    ACTION( REQ_LIST_GET,     "List.get",       2,      2  )\
+    ACTION( REQ_LIST_INSERT,  "List.insert",    4,      0  )\
+    ACTION( REQ_LIST_PUSH,    "List.push",      3,      -1 )
+
+typedef enum list_elem {
+    LIST_VERB = 0,
+    LIST_KEY = 1,
+} list_elem_e;
