6a5eb75c2fd31944bfbe58642b4cf2106db0a067
Jonathan D. Simms <jsimms@twitter.com>
2018-06-21 20:31:59 -0400

    generate both static and shared targets

--- cmake/CMakeCargo.cmake
+++ cmake/CMakeCargo.cmake
@@ -46,13 +46,13 @@ function(cargo_build)
         set(LIB_BUILD_TYPE "debug")
     endif()
 
-    if(RUST_CDYLIB_BUILD)
-        set(LIB_FNAME "${CMAKE_STATIC_LIBRARY_PREFIX}${LIB_NAME}${CMAKE_STATIC_LIBRARY_SUFFIX}")
-    else()
-        set(LIB_FNAME "${CMAKE_DYNAMIC_LIBRARY_PREFIX}${LIB_NAME}${CMAKE_DYNAMIC_LIBRARY_SUFFIX}")
-    endif()
+    set(SHARED_LIB_FNAME "${CMAKE_SHARED_LIBRARY_PREFIX}${LIB_NAME}${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    set(STATIC_LIB_FNAME "${CMAKE_STATIC_LIBRARY_PREFIX}${LIB_NAME}${CMAKE_STATIC_LIBRARY_SUFFIX}")
 
-    set(LIB_FILE "${CARGO_TARGET_DIR}/${LIB_TARGET}/${LIB_BUILD_TYPE}/${LIB_FNAME}")
+    set(LIB_BASE_DIR "${CARGO_TARGET_DIR}/${LIB_TARGET}/${LIB_BUILD_TYPE}")
+
+    set(SHARED_LIB_FILE "${LIB_BASE_DIR}/${SHARED_LIB_FNAME}")
+    set(STATIC_LIB_FILE "${LIB_BASE_DIR}/${STATIC_LIB_FNAME}")
 
 	if(IOS)
 		set(CARGO_ARGS "lipo")
@@ -70,13 +70,25 @@ function(cargo_build)
     set(CARGO_ENV_COMMAND ${CMAKE_COMMAND} -E env "CARGO_TARGET_DIR=${CARGO_TARGET_DIR}")
 
     add_custom_command(
-        OUTPUT ${LIB_FILE}
+        OUTPUT ${STATIC_LIB_FILE}
         COMMAND ${CARGO_ENV_COMMAND} ${CARGO_EXECUTABLE} ARGS ${CARGO_ARGS}
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         DEPENDS ${LIB_SOURCES}
         COMMENT "running cargo")
-    add_custom_target(${CARGO_NAME}_target ALL DEPENDS ${LIB_FILE})
-    add_library(${CARGO_NAME} STATIC IMPORTED GLOBAL)
-    add_dependencies(${CARGO_NAME} ${CARGO_NAME}_target)
-    set_target_properties(${CARGO_NAME} PROPERTIES IMPORTED_LOCATION ${LIB_FILE})
+    add_custom_target(${CARGO_NAME}_static_target ALL DEPENDS ${STATIC_LIB_FILE})
+    add_library(${CARGO_NAME}_static STATIC IMPORTED GLOBAL)
+    add_dependencies(${CARGO_NAME}_static ${CARGO_NAME}_static_target)
+    set_target_properties(${CARGO_NAME}_static PROPERTIES IMPORTED_LOCATION ${STATIC_LIB_FILE})
+
+    add_custom_command(
+        OUTPUT ${SHARED_LIB_FILE}
+        COMMAND ${CARGO_ENV_COMMAND} ${CARGO_EXECUTABLE} ARGS ${CARGO_ARGS}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        DEPENDS ${LIB_SOURCES}
+        COMMENT "running cargo")
+    add_custom_target(${CARGO_NAME}_shared_target ALL DEPENDS ${SHARED_LIB_FILE})
+    add_library(${CARGO_NAME}_shared SHARED IMPORTED GLOBAL)
+    add_dependencies(${CARGO_NAME}_shared ${CARGO_NAME}_shared_target)
+    set_target_properties(${CARGO_NAME}_shared PROPERTIES IMPORTED_LOCATION ${SHARED_LIB_FILE})
+
 endfunction()
