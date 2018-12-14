8a9ecc2d9bd809f01552b2deb9c921c4f61c907c
Yao Yue <thinkingfish@gmail.com>
2018-12-14 09:20:38 -0800

    Ccommon 2.0.0 (#205)
    * Squashed 'deps/ccommon/' changes from f5efe29..2ef0163
    
    2ef0163 Reorder dependency includes in cmake, don't parallel build (#182)
    a6a54d9 remove endian-specific logic from str*cmp (#177)
    4c0668b epoll_create* ignores size hint in newer kernels, switch to new API (#179)
    c9c5ee5 improve cc_bstring string literal and cstring names (#176)
    0184d73 Add unit tests for buffer, fix buf/dbuf bugs and refactor (#174)
    d7dab43 create a .cargo/config so intellij uses the same target dir as cmake (#173)
    e710712 use accept4 for tcp_accept when available (#171)
    21ba10e Remove cargo lock for shared lib, closes #169 (#172)
    24660f1 update style guide (#170)
    17baf1e Per thread logging (#168)
    
    git-subtree-dir: deps/ccommon
    git-subtree-split: 2ef016304ea70f8562dfc01126a452c41fbe2bc6
    
    * fix bstring merge error
    
    * update rust related build scripts and cdb_rs module naming
    
    * Squashed 'deps/ccommon/' changes from 2ef0163..2168fec
    
    2168fec minimize osx build config (#185)
    42b24de Simplify rust options, specify fewer output targets (#183)
    c9fa905 update CMakeRust used to latest version, tweaks to make build work (#184)
    
    git-subtree-dir: deps/ccommon
    git-subtree-split: 2168feca7bd2cf9715fe2c95f5f6e6e6495f3795
    
    * small adjustments
    
    * update CI
    
    * update before-install.sh
    
    * fixing integration test seq after switching to unix style expiry time

--- cmake/FindRust.cmake
+++ cmake/FindRust.cmake
@@ -4,55 +4,53 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
 set(_CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ${CMAKE_FIND_ROOT_PATH_MODE_INCLUDE})
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
 
-if(WIN32)
+if(CMAKE_HOST_WIN32)
 	set(USER_HOME "$ENV{USERPROFILE}")
 else()
 	set(USER_HOME "$ENV{HOME}")
 endif()
 
-# Find cargo prefix
-find_path(CARGO_PREFIX ".cargo"
-	PATHS "${USER_HOME}")
-
-if(CARGO_PREFIX MATCHES "NOTFOUND")
-	message(FATAL_ERROR "Could not find Rust!")
-else()
-	set(CARGO_PREFIX "${CARGO_PREFIX}/.cargo")
+if(NOT DEFINED CARGO_HOME)
+	if("$ENV{CARGO_HOME}" STREQUAL "")
+		set(CARGO_HOME "${USER_HOME}/.cargo")
+	else()
+		set(CARGO_HOME "$ENV{CARGO_HOME}")
+	endif()
 endif()
 
 # Find cargo executable
 find_program(CARGO_EXECUTABLE cargo
-	HINTS "${CARGO_PREFIX}"
+	HINTS "${CARGO_HOME}"
 	PATH_SUFFIXES "bin")
 mark_as_advanced(CARGO_EXECUTABLE)
 
 # Find rustc executable
 find_program(RUSTC_EXECUTABLE rustc
-	HINTS "${CARGO_PREFIX}"
+	HINTS "${CARGO_HOME}"
 	PATH_SUFFIXES "bin")
 mark_as_advanced(RUSTC_EXECUTABLE)
 
 # Find rustdoc executable
 find_program(RUSTDOC_EXECUTABLE rustdoc
-	HINTS "${CARGO_PREFIX}"
+	HINTS "${CARGO_HOME}"
 	PATH_SUFFIXES "bin")
 mark_as_advanced(RUSTDOC_EXECUTABLE)
 
 # Find rust-gdb executable
 find_program(RUST_GDB_EXECUTABLE rust-gdb
-	HINTS "${CARGO_PREFIX}"
+	HINTS "${CARGO_HOME}"
 	PATH_SUFFIXES "bin")
 mark_as_advanced(RUST_GDB_EXECUTABLE)
 
 # Find rust-lldb executable
 find_program(RUST_LLDB_EXECUTABLE rust-lldb
-	HINTS "${CARGO_PREFIX}"
+	HINTS "${CARGO_HOME}"
 	PATH_SUFFIXES "bin")
 mark_as_advanced(RUST_LLDB_EXECUTABLE)
 
 # Find rustup executable
 find_program(RUSTUP_EXECUTABLE rustup
-	HINTS "${CARGO_PREFIX}"
+	HINTS "${CARGO_HOME}"
 	PATH_SUFFIXES "bin")
 mark_as_advanced(RUSTUP_EXECUTABLE)
 
@@ -61,7 +59,7 @@ set(RUST_FOUND FALSE CACHE INTERNAL "")
 if(CARGO_EXECUTABLE AND RUSTC_EXECUTABLE AND RUSTDOC_EXECUTABLE)
 	set(RUST_FOUND TRUE CACHE INTERNAL "")
 
-	set(CARGO_PREFIX "${CARGO_PREFIX}" CACHE PATH "Rust Cargo prefix")
+	set(CARGO_HOME "${CARGO_HOME}" CACHE PATH "Rust Cargo Home")
 
 	execute_process(COMMAND ${RUSTC_EXECUTABLE} --version OUTPUT_VARIABLE RUSTC_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
 	string(REGEX REPLACE "rustc ([^ ]+) .*" "\\1" RUSTC_VERSION "${RUSTC_VERSION}")
