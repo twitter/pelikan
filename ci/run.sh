twitter/pelikan

commit 14fcd34ccbbfdacc4eb28e66dc1a54e31339415d

fix check related config

CMakeLists.txt
@@ -116,6 +116,12 @@ if(BUILD_AND_INSTALL_CHECK)
     set(CMAKE_REQUIRED_LIBRARIES "${CHECK_ROOT_DIR}/lib")
 endif()
 
+if(NOT CHECK_FOUND)
+    find_package(Check QUIET 0.10)
+endif()
+
+
+
 if(${OS_PLATFORM} MATCHES "OS_LINUX")
   set(CFLAGS_LIST "${CFLAGS_LIST} -lrt")
 endif()
@@ -144,10 +150,6 @@ find_package(PkgConfig QUIET)
 
 if(PKG_CONFIG_FOUND)
     pkg_check_modules(CHECK QUIET check>=0.10)
-endif()
-
-if(NOT CHECK_FOUND)
-    find_package(Check QUIET 0.10)
 endif()
 
 if (USE_PMEM)
@@ -179,11 +181,12 @@ add_subdirectory(src)
 
 # tests: always build last
 if(CHECK_FOUND)
-    include_directories(${include_directories} ${CHECK_INCLUDES})
+    include_directories(${CHECK_INCLUDE_DIRS})
+    link_directories(${CHECK_LIBRARY_DIRS})
     add_subdirectory(test)
 endif(CHECK_FOUND)
 
-add_subdirectory(benchmarks)
+# add_subdirectory(benchmarks)
 
 # print a summary
 

test/CMakeLists.txt
@@ -1,7 +1,5 @@
 include_directories(${include_directories} CHECK_INCLUDES)
 
-add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND})
-
 add_subdirectory(data_structure)
 add_subdirectory(protocol)
 add_subdirectory(hotkey)

test/data_structure/bitmap/CMakeLists.txt
@@ -8,5 +8,4 @@ target_link_libraries(${test_name} ds_${suite})
 target_link_libraries(${test_name} ccommon-static ${CHECK_LIBRARIES})
 target_link_libraries(${test_name} pthread m)
 
-add_dependencies(check ${test_name})
 add_test(${test_name} ${test_name})

test/data_structure/intarray/CMakeLists.txt
@@ -7,5 +7,4 @@ add_executable(${test_name} ${source})
 target_link_libraries(${test_name} ds_${suite})
 target_link_libraries(${test_name} ccommon-static ${CHECK_LIBRARIES} pthread m)
 
-add_dependencies(check ${test_name})
 add_test(${test_name} ${test_name})

test/data_structure/ziplist/CMakeLists.txt
@@ -7,5 +7,4 @@ add_executable(${test_name} ${source})
 target_link_libraries(${test_name} ds_${suite})
 target_link_libraries(${test_name} ccommon-static ${CHECK_LIBRARIES} pthread m)
 
-add_dependencies(check ${test_name})
 add_test(${test_name} ${test_name})

test/datapool/CMakeLists.txt
@@ -8,5 +8,4 @@ target_link_libraries(${test_name} ${suite})
 target_link_libraries(${test_name} ccommon-static ${CHECK_LIBRARIES})
 target_link_libraries(${test_name} datapool)
 
-add_dependencies(check ${test_name})
 add_test(${test_name} ${test_name})

test/hotkey/kc_map/CMakeLists.txt
@@ -7,5 +7,4 @@ add_executable(${test_name} ${source})
 target_link_libraries(${test_name} hotkey)
 target_link_libraries(${test_name} ccommon-static ${CHECK_LIBRARIES} pthread m)
 
-add_dependencies(check ${test_name})
 add_test(${test_name} ${test_name})

test/hotkey/key_window/CMakeLists.txt
@@ -7,5 +7,4 @@ add_executable(${test_name} ${source})
 target_link_libraries(${test_name} hotkey)
 target_link_libraries(${test_name} ccommon-static ${CHECK_LIBRARIES} pthread m)
 
-add_dependencies(check ${test_name})
 add_test(${test_name} ${test_name})

commit 949dc7e3967716c7677d76752bb8c22a43ed9d03

Fix testing configuration
- use make test instead of make check (fix regression introduced in
14fcd34)
- Fix Cmake warning "Policy CMP0075" by remove CMAKE_REQUIRED_LIBRARIES

CMakeLists.txt
@@ -114,7 +114,6 @@ if(BUILD_AND_INSTALL_CHECK)
 
     set(CHECK_ROOT_DIR "${LIBCHECK_PREFIX}")
     set(CMAKE_REQUIRED_INCLUDES "${CHECK_ROOT_DIR}/include")    # these make check link correctly in ccommon and pelikan
-    set(CMAKE_REQUIRED_LIBRARIES "${CHECK_ROOT_DIR}/lib")
 endif()
 
 find_package(PkgConfig QUIET)

ci/run.sh
@@ -23,7 +23,7 @@ fi
 
 # TODO: run cmake3 on centos hosts
 
-mkdir -p _build && ( cd _build && cmake ${CMAKE_ARGS[@]} .. && make && make check ) || die 'make failed'
+mkdir -p _build && ( cd _build && cmake ${CMAKE_ARGS[@]} .. && make && make test ) || die 'make failed'
 
 egrep -r ":F:|:E:" . |grep -v 'Binary file' || true
 