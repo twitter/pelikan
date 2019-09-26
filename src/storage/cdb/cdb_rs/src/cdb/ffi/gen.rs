f47e9f26a563da28459e80b5f3342a49b39baa46
Sean Lynch <slynch@twitter.com>
2019-09-26 11:23:35 -0700

    Silence improper_ctypes warning in cdb
    This warning was causing so much output to be generated that
    it was causing CI to fail

--- src/storage/cdb/cdb_rs/src/cdb/ffi/gen.rs
+++ src/storage/cdb/cdb_rs/src/cdb/ffi/gen.rs
@@ -5,4 +5,10 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
+
+// TODO(sean): fix the build script here so that it only generates the necessary bindings
+// Note: This is a hack to get CI to pass, otherwise the warnings
+// are so long that travis fails the build due to excessive output.
+#![allow(improper_ctypes)]
+
 include!(concat!(env!("OUT_DIR"), "/ffigen.rs"));
