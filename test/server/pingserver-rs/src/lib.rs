7a5c57785a2a9b9e39bb466b9806461aa9bdabe3
Sean Lynch <slynch@twitter.com>
2019-11-14 14:05:24 -0800

    Add license headers

--- test/server/pingserver-rs/src/lib.rs
+++ test/server/pingserver-rs/src/lib.rs
@@ -1,3 +1,17 @@
+// Copyright (C) 2019 Twitter, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::env;
 use std::fmt;
 use std::io::{Read, Result as IOResult, Write};
@@ -120,6 +134,8 @@ fn admin_crash() -> IOResult<()> {
     stream.set_nodelay(true)?;
     stream.set_nonblocking(false)?;
 
+    /// This set of commands exposed a use-after-free bug in the
+    /// admin handling thread.
     stream.write_all(b"stats\r\n")?;
     stream.write_all(b"\
         Bacon ipsum dolor amet tongue rump pork belly, \
