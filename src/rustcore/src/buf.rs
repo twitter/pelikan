08f0d094633934a7ccbb52e3d6d427329201d5e9
Sean Lynch <slynch@twitter.com>
2019-11-21 14:22:13 -0800

    Remove some unsafe code within rustcore

--- src/rustcore/src/buf.rs
+++ src/rustcore/src/buf.rs
@@ -21,7 +21,7 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 
 /// Create a future for `poll_read_buf` in `AsyncRead`.
-pub fn read_buf<'a, A: AsyncRead, B: BufMut + Unpin>(
+pub fn read_buf<'a, A: AsyncRead + Unpin, B: BufMut + Unpin>(
     read: &'a mut A,
     buf: &'a mut B,
 ) -> impl Future<Output = Result<usize>> + 'a {
@@ -29,61 +29,49 @@ pub fn read_buf<'a, A: AsyncRead, B: BufMut + Unpin>(
 }
 
 /// Create a future for `poll_write_buf` in `AsyncWrite`.
-pub fn write_buf<'a, A: AsyncWrite, B: Buf + Unpin>(
+pub fn write_buf<'a, A: AsyncWrite + Unpin, B: Buf + Unpin>(
     write: &'a mut A,
     buf: &'a mut B,
 ) -> impl Future<Output = Result<usize>> + 'a {
     WriteBuf::new(write, buf)
 }
 
-struct ReadBuf<'a, A: AsyncRead, B: BufMut> {
+struct ReadBuf<'a, A: AsyncRead + Unpin, B: BufMut> {
     read: &'a mut A,
     buf: &'a mut B,
 }
 
-impl<'a, A: AsyncRead, B: BufMut> ReadBuf<'a, A, B> {
+impl<'a, A: AsyncRead + Unpin, B: BufMut> ReadBuf<'a, A, B> {
     pub fn new(read: &'a mut A, buf: &'a mut B) -> Self {
         Self { read, buf }
     }
 }
 
-impl<'a, A: AsyncRead, B: BufMut + Unpin> Future for ReadBuf<'a, A, B> {
+impl<'a, A: AsyncRead + Unpin, B: BufMut + Unpin> Future for ReadBuf<'a, A, B> {
     type Output = Result<usize>;
 
-    fn poll(self: Pin<&mut Self>, ctx: &mut Context) -> Poll<Self::Output> {
-        // Safe since we are only performing pin projection and
-        // self.buf is Unpin
-        let (read_pin, buf) = unsafe {
-            let me = Pin::into_inner_unchecked(self);
-            (Pin::new_unchecked(&mut *me.read), &mut *me.buf)
-        };
-
-        read_pin.poll_read_buf(ctx, buf)
+    fn poll(mut self: Pin<&mut Self>, ctx: &mut Context) -> Poll<Self::Output> {
+        let Self { read, buf } = &mut *self;
+        Pin::new(read).poll_read_buf(ctx, buf)
     }
 }
 
-struct WriteBuf<'a, A: AsyncWrite, B: Buf> {
+struct WriteBuf<'a, A: AsyncWrite + Unpin, B: Buf> {
     write: &'a mut A,
     buf: &'a mut B,
 }
 
-impl<'a, A: AsyncWrite, B: Buf> WriteBuf<'a, A, B> {
+impl<'a, A: AsyncWrite + Unpin, B: Buf> WriteBuf<'a, A, B> {
     pub fn new(write: &'a mut A, buf: &'a mut B) -> Self {
         Self { write, buf }
     }
 }
 
-impl<'a, A: AsyncWrite, B: Buf + Unpin> Future for WriteBuf<'a, A, B> {
+impl<'a, A: AsyncWrite + Unpin, B: Buf + Unpin> Future for WriteBuf<'a, A, B> {
     type Output = Result<usize>;
 
-    fn poll(self: Pin<&mut Self>, ctx: &mut Context) -> Poll<Self::Output> {
-        // Safe since we are only performing pin projection
-        // and self.buf is unpin.
-        let (write_pin, buf) = unsafe {
-            let me = Pin::into_inner_unchecked(self);
-            (Pin::new_unchecked(&mut *me.write), &mut *me.buf)
-        };
-
-        write_pin.poll_write_buf(ctx, buf)
+    fn poll(mut self: Pin<&mut Self>, ctx: &mut Context) -> Poll<Self::Output> {
+        let Self { write, buf } = &mut *self;
+        Pin::new(write).poll_write_buf(ctx, buf)
     }
 }
