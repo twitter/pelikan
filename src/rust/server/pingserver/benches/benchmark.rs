b4e7743d0a26ad6e90299d341729dd707f4a09cc
Brian Martin <bmartin@twitter.com>
2021-06-02 10:34:27 -0700

    fix pingserver benchmark

--- src/rust/server/pingserver/benches/benchmark.rs
+++ src/rust/server/pingserver/benches/benchmark.rs
@@ -11,16 +11,19 @@
 // For formal performance testing, it is recommended to use
 // https://github.com/twitter/rpc-perf or another memcached benchmarking tool.
 
+use config::PingserverConfig;
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
-use pelikan_segcache_rs::TwemcacheBuilder;
+use pelikan_pingserver_rs::Pingserver;
 
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::time::Duration;
 
-fn get_benchmark(c: &mut Criterion) {
+fn ping_benchmark(c: &mut Criterion) {
+    let config = PingserverConfig::default();
+
     // launch the server
-    let server = TwemcacheBuilder::default().spawn();
+    let server = Pingserver::new(config);
 
     // wait for server to startup. duration is chosen to be longer than we'd
     // expect startup to take in a slow ci environment.
@@ -34,60 +37,25 @@ fn get_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("request");
     group.throughput(Throughput::Elements(1));
 
-    // benchmark for a few key lengths
-    for klen in [1, 16, 64, 255].iter() {
-        // benchmark getting empty value
-        let bench_name = format!("get/{}b/0b", klen);
-        let key = format!("{:01$}", 0, klen);
-        let msg = format!("get {}\r\n", key);
-        group.bench_function(&bench_name, |b| {
-            b.iter(|| {
-                assert!(stream.write_all(msg.as_bytes()).is_ok());
-                if let Ok(bytes) = stream.read(&mut buffer) {
-                    if &buffer[0..bytes] != b"END\r\n" {
-                        panic!("invalid response");
-                    }
-                } else {
-                    panic!("read error");
-                }
-            })
-        });
+    let msg = b"ping\r\n";
+    let bench_name = "ping";
 
-        // benchmark across a few value lengths
-        for (key, vlen) in [1, 64, 1024, 4096].iter().enumerate() {
-            let key = format!("{:01$}", key, klen);
-            let value = format!("{:01$}", 0, vlen);
-            let msg = format!("set {} 0 0 {}\r\n{}\r\n", key, vlen, value);
-            assert!(stream.write_all(msg.as_bytes()).is_ok());
+    group.bench_function(bench_name, |b| {
+        b.iter(|| {
+            assert!(stream.write_all(msg).is_ok());
             if let Ok(bytes) = stream.read(&mut buffer) {
-                if &buffer[0..bytes] != b"STORED\r\n" {
-                    panic!("invalid response: {:?}", &buffer[0..bytes]);
+                if &buffer[0..bytes] != b"PONG\r\n" {
+                    panic!("invalid response");
                 }
             } else {
                 panic!("read error");
             }
-
-            let bench_name = format!("get/{}b/{}b", klen, vlen);
-            let msg = format!("get {}\r\n", key);
-            let response = format!("VALUE {} 0 {}\r\n{}\r\nEND\r\n", key, vlen, value);
-            group.bench_function(&bench_name, |b| {
-                b.iter(|| {
-                    assert!(stream.write_all(msg.as_bytes()).is_ok());
-                    if let Ok(bytes) = stream.read(&mut buffer) {
-                        if &buffer[0..bytes] != response.as_bytes() {
-                            panic!("invalid response");
-                        }
-                    } else {
-                        panic!("read error");
-                    }
-                })
-            });
-        }
-    }
+        })
+    });
 
     // shutdown the server
     server.shutdown();
 }
 
-criterion_group!(benches, get_benchmark);
+criterion_group!(benches, ping_benchmark);
 criterion_main!(benches);
