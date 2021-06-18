90a87721733aa60fd10c2d73c9dbb536c16dd877
Brian Martin <bmartin@twitter.com>
2021-06-18 07:45:01 -0700

    add missing copyright headers to source files

--- src/rust/storage/seg/src/hashtable/hash_bucket.rs
+++ src/rust/storage/seg/src/hashtable/hash_bucket.rs
@@ -1,3 +1,7 @@
+// Copyright 2021 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 //! Hash buckets are used to store a group of item entries with a shared
 //! metadata entry.
 //!
