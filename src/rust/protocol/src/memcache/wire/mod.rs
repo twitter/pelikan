48f708e48d2f0adeaa78e0b551f6939bb6d85ecb
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2021-09-04 21:41:52 -0400

    Duplicate metrics for src/rust/protocol

--- src/rust/protocol/src/memcache/wire/mod.rs
+++ src/rust/protocol/src/memcache/wire/mod.rs
@@ -12,6 +12,33 @@ use super::*;
 use crate::*;
 
 use metrics::Stat;
+use metrics::{pelikan_metrics, Counter};
+
+pelikan_metrics! {
+    static GET: Counter;
+    static GET_KEY: Counter;
+    static GET_KEY_HIT: Counter;
+    static GET_KEY_MISS: Counter;
+    
+    static GETS: Counter;
+    static GETS_KEY: Counter;
+    static GETS_KEY_HIT: Counter;
+    static GETS_KEY_MISS: Counter;
+
+    static SET: Counter;
+    static SET_STORED: Counter;
+    static SET_NOT_STORED: Counter;
+
+    static ADD: Counter;
+    static ADD_STORED: Counter;
+    static ADD_NOT_STORED: Counter;
+
+    static REPLACE: Counter;
+
+    static DELETE: Counter;
+
+    static CAS: Counter;
+}
 
 impl<'a, T> Execute<MemcacheRequest, MemcacheResponse> for T
 where
@@ -21,8 +48,13 @@ where
         let response = match request {
             MemcacheRequest::Get { keys } => {
                 increment_counter!(&Stat::Get);
+                GET.increment();
 
                 let entries = self.get(&keys);
+
+                GET_KEY.add(keys.len() as _);
+                GET_KEY_HIT.add(entries.len() as _);
+                GET_KEY_MISS.add(keys.len() as _);
 
                 increment_counter_by!(&Stat::GetKey, keys.len() as u64);
                 increment_counter_by!(&Stat::GetKeyHit, entries.len() as u64);
@@ -35,8 +67,13 @@ where
             }
             MemcacheRequest::Gets { keys } => {
                 increment_counter!(&Stat::Gets);
+                GETS.increment();
 
                 let entries = self.get(&keys);
+
+                GETS_KEY.add(keys.len() as _);
+                GETS_KEY_HIT.add(keys.len() as _);
+                GETS_KEY_MISS.add(keys.len() as _);
 
                 increment_counter_by!(&Stat::GetsKey, keys.len() as u64);
                 increment_counter_by!(&Stat::GetsKeyHit, entries.len() as u64);
@@ -46,13 +83,17 @@ where
             }
             MemcacheRequest::Set { entry, noreply } => {
                 increment_counter!(&Stat::Set);
+                SET.increment();
+
                 let response = match self.set(entry) {
                     Ok(_) => {
                         increment_counter!(&Stat::SetStored);
+                        SET_STORED.increment();
                         MemcacheResponse::Stored
                     }
                     Err(MemcacheStorageError::NotStored) => {
                         increment_counter!(&Stat::SetNotstored);
+                        SET_NOT_STORED.increment();
                         MemcacheResponse::NotStored
                     }
                     _ => {
@@ -66,13 +107,17 @@ where
             }
             MemcacheRequest::Add { entry, noreply } => {
                 increment_counter!(&Stat::Add);
+                ADD.increment();
+
                 let response = match self.add(entry) {
                     Ok(_) => {
                         increment_counter!(&Stat::AddStored);
+                        ADD_STORED.increment();
                         MemcacheResponse::Stored
                     }
                     Err(MemcacheStorageError::NotStored) => {
                         increment_counter!(&Stat::AddNotstored);
+                        ADD_NOT_STORED.increment();
                         MemcacheResponse::NotStored
                     }
                     _ => {
@@ -86,6 +131,7 @@ where
             }
             MemcacheRequest::Replace { entry, noreply } => {
                 increment_counter!(&Stat::Replace);
+                REPLACE.increment();
                 let response = match self.replace(entry) {
                     Ok(_) => MemcacheResponse::Stored,
                     Err(MemcacheStorageError::NotStored) => MemcacheResponse::NotStored,
@@ -100,6 +146,7 @@ where
             }
             MemcacheRequest::Delete { key, noreply } => {
                 increment_counter!(&Stat::Delete);
+                DELETE.increment();
                 let response = match self.delete(&key) {
                     Ok(_) => MemcacheResponse::Deleted,
                     Err(MemcacheStorageError::NotFound) => MemcacheResponse::NotFound,
@@ -114,6 +161,7 @@ where
             }
             MemcacheRequest::Cas { entry, noreply } => {
                 increment_counter!(&Stat::Cas);
+                CAS.increment();
                 let response = match self.cas(entry) {
                     Ok(_) => MemcacheResponse::Stored,
                     Err(MemcacheStorageError::NotFound) => MemcacheResponse::NotFound,
