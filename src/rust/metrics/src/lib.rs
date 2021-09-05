fa2c65d45fdc03cb573538ca507a7f2121c7985a
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2021-09-04 22:45:50 -0400

    Remove fastmetrics from metrics crate

--- src/rust/metrics/src/lib.rs
+++ src/rust/metrics/src/lib.rs
@@ -2,14 +2,10 @@
 // Licensed under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
-pub use rustcommon_fastmetrics::*;
-use strum::IntoEnumIterator;
-use strum_macros::{AsRefStr, EnumIter};
+// pub use rustcommon_fastmetrics::*;
 
 pub use macros::to_lowercase;
 pub use rustcommon_metrics::{metric, Counter, Gauge};
-
-pub type Metrics = rustcommon_fastmetrics::Metrics<Stat>;
 
 #[doc(hidden)]
 pub extern crate rustcommon_metrics;
@@ -50,183 +46,10 @@ macro_rules! test_no_duplicates {
     };
 }
 
-// As a temporary work-around for requiring all metrics to have a common type,
-// we combine server and storage metrics here.
-
-/// Defines various statistics
-#[derive(Copy, Clone, Debug, AsRefStr, EnumIter)]
-#[strum(serialize_all = "snake_case")]
-#[deprecated]
-pub enum Stat {
-    // server/twemcache-rs
-    Add,
-    AddNotstored,
-    AddStored,
-    AdminEventError,
-    AdminEventLoop,
-    AdminEventRead,
-    AdminEventTotal,
-    AdminEventWrite,
-    AdminRequestParse,
-    AdminRequestParseEx,
-    AdminResponseCompose,
-    AdminResponseComposeEx,
-    Cas,
-    CasNotfound,
-    CasExists,
-    CasEx,
-    CasStored,
-    Delete,
-    DeleteDeleted,
-    DeleteNotfound,
-    Get,
-    GetKey,
-    GetKeyHit,
-    GetKeyMiss,
-    GetEx,
-    Gets,
-    GetsKey,
-    GetsKeyHit,
-    GetsKeyMiss,
-    GetsEx,
-    Set,
-    SetNotstored,
-    SetStored,
-    SetEx,
-    Pid,
-    ProcessReq,
-    ProcessEx,
-    ProcessServerEx,
-    Replace,
-    ReplaceNotstored,
-    ReplaceStored,
-    RequestParse,
-    RequestParseEx,
-    ResponseCompose,
-    ResponseComposeEx,
-    RuUtime,
-    RuStime,
-    RuMaxrss,
-    RuIxrss,
-    RuIdrss,
-    RuIsrss,
-    RuMinflt,
-    RuMajflt,
-    RuNswap,
-    RuInblock,
-    RuOublock,
-    RuMsgsnd,
-    RuMsgrcv,
-    RuNsignals,
-    RuNvcsw,
-    RuNivcsw,
-    ServerEventError,
-    ServerEventLoop,
-    ServerEventRead,
-    ServerEventTotal,
-    ServerEventWrite,
-    SessionRecv,
-    SessionRecvByte,
-    SessionRecvEx,
-    SessionSend,
-    SessionSendByte,
-    SessionSendEx,
-    StorageEventLoop,
-    TcpAccept,
-    TcpAcceptEx,
-    TcpClose,
-    TcpConnect,
-    TcpConnectEx,
-    TcpRecv,
-    TcpRecvByte,
-    TcpRecvEx,
-    TcpReject,
-    TcpRejectEx,
-    TcpSend,
-    TcpSendByte,
-    TcpSendEx,
-    TcpSendPartial,
-    WorkerEventError,
-    WorkerEventLoop,
-    WorkerEventRead,
-    WorkerEventTotal,
-    WorkerEventWake,
-    WorkerEventWrite,
-
-    // storage/segcache
-    ExpireTime,       // total time spent in expiration, nanoseconds
-    HashLookup,       // total number of hash lookups
-    HashInsert,       // total number of inserts
-    HashInsertEx,     // caused by insert exceptions, typically ENOSPC
-    HashRemove,       // number of hash removals
-    HashTagCollision, // number of collisions
-    HashArrayAlloc,
-    ItemCurrent,
-    ItemCurrentBytes,
-    ItemAlloc,
-    ItemAllocEx,
-    ItemCompacted, // TODO(bmartin): better name?
-    ItemDead,
-    ItemDeadBytes,
-    ItemDelete,
-    ItemEvict,
-    ItemExpire,
-    ItemRelink,
-    ItemReplace,
-    SegmentRequest,
-    SegmentRequestEx,
-    SegmentReturn,
-    SegmentEvict,
-    SegmentEvictRetry,
-    SegmentEvictEx,
-    SegmentExpire,
-    SegmentMerge,
-    SegmentCurrent,
-    SegmentFree,
-}
-
-#[allow(clippy::from_over_into)]
-impl Into<usize> for Stat {
-    fn into(self) -> usize {
-        self as usize
-    }
-}
-
-impl std::fmt::Display for Stat {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.as_ref())
-    }
-}
-
-impl Metric for Stat {
-    fn source(&self) -> Source {
-        match self {
-            Stat::Pid
-            | Stat::RuMaxrss
-            | Stat::RuIxrss
-            | Stat::RuIdrss
-            | Stat::RuIsrss
-            | Stat::SegmentCurrent
-            | Stat::SegmentFree
-            | Stat::ItemCurrent
-            | Stat::ItemCurrentBytes
-            | Stat::ItemDead
-            | Stat::ItemDeadBytes => Source::Gauge,
-            _ => Source::Counter,
-        }
-    }
-
-    fn index(&self) -> usize {
-        (*self).into()
-    }
+pelikan_metrics! {
+    static PID: Gauge;
 }
 
 pub fn init() {
-    let metrics: Vec<Stat> = Stat::iter().collect();
-    MetricsBuilder::<Stat>::new()
-        .metrics(&metrics)
-        .build()
-        .unwrap();
-
-    set_gauge!(&Stat::Pid, std::process::id().into());
+    PID.set(std::process::id().into());
 }
