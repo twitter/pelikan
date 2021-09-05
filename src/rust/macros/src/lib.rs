908dec013e92f84cf2a8c09fdbbfc7a2a821811e
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2021-09-04 20:55:29 -0400

    Add proc-macro for converting an ident to a lowercase string

---
+++ src/rust/macros/src/lib.rs
@@ -0,0 +1,20 @@
+
+use proc_macro::TokenStream;
+use syn::Ident;
+use quote::quote;
+
+/// This macro statically converts an ident to a lowercased string
+/// at compile time.
+/// 
+/// In the future this could be replaced with some const code. However,
+/// `std::str::from_utf8_unchecked` is not stably const just yet so we
+/// need this macro as a workaround.
+#[proc_macro]
+pub fn to_lowercase(input: TokenStream) -> TokenStream {
+    let ident = syn::parse_macro_input!(input as Ident);
+    let name = ident.to_string().to_ascii_lowercase();
+    let literal = syn::LitStr::new(&name, ident.span());
+    let tokens = quote!{ #literal };
+
+    tokens.into()
+}
