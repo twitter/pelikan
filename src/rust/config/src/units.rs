6fee0c4bd661c0cc6f2eb234108859973b941415
Brian Martin <bmartin@twitter.com>
2021-09-09 15:43:09 -0700

    add license header

--- src/rust/config/src/units.rs
+++ src/rust/config/src/units.rs
@@ -1,3 +1,7 @@
+// Copyright 2021 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 pub const B: usize = 1;
 pub const KB: usize = 1024 * B;
 pub const MB: usize = 1024 * KB;
