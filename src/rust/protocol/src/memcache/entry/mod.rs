d9ffc4b70997104ab34f5b692a906d8dca5fac0d
Brian Martin <bmartin@twitter.com>
2022-01-25 09:47:19 -0800

    rustfmt

--- src/rust/protocol/src/memcache/entry/mod.rs
+++ src/rust/protocol/src/memcache/entry/mod.rs
@@ -2,8 +2,8 @@
 // Licensed under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
-use storage_types::{OwnedValue, Value};
 use std::time::Duration;
+use storage_types::{OwnedValue, Value};
 
 #[derive(Debug)]
 pub struct MemcacheEntry {
