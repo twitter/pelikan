3d614552c25c6e1cdbb34ed618afd72d1d0f1a3c
Brian Martin <bmartin@twitter.com>
2022-06-17 11:38:08 -0700

    expand fuzz testing to cover more of the API

--- src/rust/storage/seg/fuzz/fuzz_targets/seg.rs
+++ src/rust/storage/seg/fuzz/fuzz_targets/seg.rs
@@ -34,6 +34,7 @@ fuzz_target!(|data: &[u8]| {
 
         match op {
             0 => {
+                // insert
                 if i >= data.len() {
                     return;
                 }
@@ -76,7 +77,7 @@ fuzz_target!(|data: &[u8]| {
                 }
                 let _ = cache.insert(key, value, None, Duration::from_secs(ttl.into()));
             }
-            1 => {
+            1 => { // delete
                 if i >= data.len() {
                     return;
                 }
@@ -97,19 +98,19 @@ fuzz_target!(|data: &[u8]| {
                 }
                 cache.delete(key);
             }
-            2 => {
+            2 => { // expire
                 if DEBUG_PRINTS {
                     println!("let _ = cache.expire()");
                 }
                 let _ = cache.expire();
             }
-            3 => {
+            3 => { // clear
                 if DEBUG_PRINTS {
                     println!("let _ = cache.clear();");
                 }
                 let _ = cache.clear();
             }
-            4 => {
+            4 => { // get
                 if i >= data.len() {
                     return;
                 }
@@ -128,6 +129,201 @@ fuzz_target!(|data: &[u8]| {
                     println!("let _ = cache.get(&{:?});", key);
                 }
                 cache.get(key);
+            }
+            5 => { // cas
+                if i >= data.len() {
+                    return;
+                }
+
+                let klen = data[i] as usize;
+                if klen == 0 {
+                    return;
+                }
+                if i + klen >= data.len() {
+                    return;
+                }
+
+                let key = &data[i..(i + klen)];
+                i += klen;
+
+                if i >= data.len() {
+                    return;
+                }
+
+                let vlen = data[i] as usize;
+                if i + vlen >= data.len() {
+                    return;
+                }
+
+                let value = &data[i..(i + vlen)];
+                i += vlen;
+
+                if i >= data.len() {
+                    return;
+                }
+
+                let ttl = data[i] as u32;
+                i += 1;
+
+                if i + 4 >= data.len() {
+                    return;
+                }
+
+                let cas = u32::from_be_bytes([data[i], data[i+1], data[i+2], data[i + 3]]);
+                i += 4;
+
+                if DEBUG_PRINTS {
+                    println!(
+                        "let _ = cache.cas(&{:?}, &{:?}, None, Duration::from_secs({}), cas);",
+                        key, value, ttl
+                    );
+                }
+                let _ = cache.cas(key, value, None, Duration::from_secs(ttl.into()), cas);
+            }
+            6 => { // incr
+                if i >= data.len() {
+                    return;
+                }
+                let klen = data[i] as usize;
+                if klen == 0 {
+                    return;
+                }
+                if i + klen >= data.len() {
+                    return;
+                }
+
+                let key = &data[i..(i + klen)];
+                i += klen;
+
+                if i + 8 >= data.len() {
+                    return;
+                }
+
+                let value = u64::from_be_bytes([data[i], data[i+1], data[i+2], data[i + 3], data[i + 4], data[i + 5], data[i + 6], data[i + 7]]);
+                i += 8;
+
+                if DEBUG_PRINTS {
+                    println!("let _ = cache.wrapping_add(&{:?}, {});", key, value);
+                }
+                let _ = cache.wrapping_add(key, value);
+            }
+            7 => { // decr
+                if i >= data.len() {
+                    return;
+                }
+                let klen = data[i] as usize;
+                if klen == 0 {
+                    return;
+                }
+                if i + klen >= data.len() {
+                    return;
+                }
+
+                let key = &data[i..(i + klen)];
+                i += klen;
+
+                if i + 8 >= data.len() {
+                    return;
+                }
+
+                let value = u64::from_be_bytes([data[i], data[i+1], data[i+2], data[i + 3], data[i + 4], data[i + 5], data[i + 6], data[i + 7]]);
+                i += 8;
+
+                if DEBUG_PRINTS {
+                    println!("let _ = cache.saturating_sub(&{:?}, {});", key, value);
+                }
+                let _ = cache.saturating_sub(key, value);
+            }
+            8 => { // insert numeric
+                if i >= data.len() {
+                    return;
+                }
+
+                let klen = data[i] as usize;
+                if klen == 0 {
+                    return;
+                }
+                if i + klen >= data.len() {
+                    return;
+                }
+
+                let key = &data[i..(i + klen)];
+                i += klen;
+
+                if i >= data.len() {
+                    return;
+                }
+
+                if i + 8 >= data.len() {
+                    return;
+                }
+
+                let value = u64::from_be_bytes([data[i], data[i+1], data[i+2], data[i + 3], data[i + 4], data[i + 5], data[i + 6], data[i + 7]]);
+                i += 8;
+
+                if i >= data.len() {
+                    return;
+                }
+
+                let ttl = data[i] as u32;
+                i += 1;
+
+                if DEBUG_PRINTS {
+                    println!(
+                        "let _ = cache.insert(&{:?}, &{:?}, None, Duration::from_secs({}));",
+                        key, value, ttl
+                    );
+                }
+                let _ = cache.insert(key, value, None, Duration::from_secs(ttl.into()));
+            }
+            9 => { // cas numeric
+                if i >= data.len() {
+                    return;
+                }
+
+                let klen = data[i] as usize;
+                if klen == 0 {
+                    return;
+                }
+                if i + klen >= data.len() {
+                    return;
+                }
+
+                let key = &data[i..(i + klen)];
+                i += klen;
+
+                if i >= data.len() {
+                    return;
+                }
+
+                if i + 8 >= data.len() {
+                    return;
+                }
+
+                let value = u64::from_be_bytes([data[i], data[i+1], data[i+2], data[i + 3], data[i + 4], data[i + 5], data[i + 6], data[i + 7]]);
+                i += 8;
+
+                if i >= data.len() {
+                    return;
+                }
+
+                let ttl = data[i] as u32;
+                i += 1;
+
+                if i + 4 >= data.len() {
+                    return;
+                }
+
+                let cas = u32::from_be_bytes([data[i], data[i+1], data[i+2], data[i + 3]]);
+                i += 4;
+
+                if DEBUG_PRINTS {
+                    println!(
+                        "let _ = cache.cas(&{:?}, &{:?}, None, Duration::from_secs({}), cas);",
+                        key, value, ttl
+                    );
+                }
+                let _ = cache.cas(key, value, None, Duration::from_secs(ttl.into()), cas);
             }
             _ => {
                 return;
