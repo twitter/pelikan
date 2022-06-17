f6ea572ed6534a53847c3c073cba1c118bcca98a
Brian Martin <bmartin@twitter.com>
2022-06-17 10:10:15 -0700

    fix: fixes seg storage bugs discovered in fuzz testing
    There was a subtle correctness bug revealed by fuzz testing where
    old item data prevented the correct initialization of new items.
    This would happen if the 'typed value' bit was seen as set by the
    previous data.
    
    This fixes the behavior by initializing the item header with zeros
    when defining a new item.
    
    Additionally fixes some dead item tracking by calling clear when
    we free a segment for having no more live items.

---
+++ src/rust/storage/seg/fuzz/fuzz_targets/seg.rs
@@ -0,0 +1,121 @@
+// Copyright 2021 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+use core::time::Duration;
+
+use seg::*;
+
+const SEG_SIZE: i32 = 1024; // 1KB
+const HEAP_SIZE: usize = 8 * 1024; // 8KB => 8 segments => 32 items total
+const HASH_POWER: u8 = 5;
+const OVERFLOW_FACTOR: f64 = 1.0;
+
+fuzz_target!(|data: &[u8]| {
+    // println!("----------");
+    let mut cache = Seg::builder()
+        .segment_size(SEG_SIZE)
+        .heap_size(HEAP_SIZE)
+        .hash_power(HASH_POWER)
+        .overflow_factor(OVERFLOW_FACTOR)
+        .build()
+        .expect("failed to create cache");
+
+    let mut i = 0;
+
+    while i < data.len() {
+        let op = data[i];
+        i += 1;
+
+        match op {
+            0 => {
+                if i < data.len() {
+                    let klen = data[i] as usize;
+                    if klen == 0 {
+                        return;
+                    }
+                    if i + klen < data.len() {
+                        let key = &data[i..(i+klen)];
+                        i += klen;
+
+                        if i < data.len() {
+                            let vlen = data[i] as usize;
+                            if i + vlen < data.len() {
+                                let value = &data[i..(i+vlen)];
+                                i += vlen;
+
+                                if i < data.len() {
+                                    let ttl = data[i] as u32;
+                                    i += 1;
+                                    // println!("let _ = cache.insert(&{:?}, &{:?}, None, Duration::from_secs({}));", key, value, ttl);
+                                    let _ = cache.insert(key, value, None, Duration::from_secs(ttl.into()));
+                                } else {
+                                    return;
+                                }
+                            } else {
+                                return;
+                            }
+                        } else {
+                            return;
+                        }
+                    } else {
+                        return;
+                    }
+                } else {
+                    return;
+                }
+            }
+            1 => {
+                if i < data.len() {
+                    let klen = data[i] as usize;
+                    if klen == 0 {
+                        return;
+                    }
+                    if i + klen < data.len() {
+                        let key = &data[i..(i+klen)];
+                        i += klen;
+                        // println!("let _ = delete(&{:?});", key);
+                        cache.delete(key);
+                    } else {
+                        return;
+                    }
+                } else {
+                    return;
+                }
+            }
+            2 => {
+                // println!("let _ = cache.expire()");
+                let _ = cache.expire();
+            }
+            3 => {
+                // println!("let _ = cache.clear()");
+                let _ = cache.clear();
+            }
+            4 => {
+                if i < data.len() {
+                    let klen = data[i] as usize;
+                    if klen == 0 {
+                        return;
+                    }
+                    if i + klen < data.len() {
+                        let key = &data[i..(i+klen)];
+                        i += klen;
+                        // println!("let _ = cache.get(&{:?});", key);
+                        cache.get(key);
+                    } else {
+                        return;
+                    }
+                } else {
+                    return;
+                }
+            }
+            _ => {
+                return;
+            }
+        }
+    }
+});
+
