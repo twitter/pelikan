169fd90aeb7024bea929c6891bbe5733606ab277
Brian Martin <bmartin@twitter.com>
2022-08-03 10:06:53 -0700

    remove TLS tests

--- src/net/src/tls_tcp.rs
+++ src/net/src/tls_tcp.rs
@@ -557,113 +557,122 @@ impl TlsTcpConnectorBuilder {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// NOTE: these tests only work if there's a `test` folder within this crate that
+// contains the necessary keys and certs. They are left here for reference and
+// in the future we should automate creation of self-signed keys and certs for
+// use for testing during local development and in CI. 
 
-    fn create_connector() -> Connector {
-        let tls_connector = TlsTcpConnector::builder()
-            .expect("failed to create builder")
-            .ca_file("test/root.crt")
-            .certificate_chain_file("test/client.crt")
-            .private_key_file("test/client.key")
-            .build()
-            .expect("failed to initialize tls connector");
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-        Connector::from(tls_connector)
-    }
+//     fn gen_keys() -> Result<(), ()> {
 
-    fn create_listener(addr: &'static str) -> Listener {
-        let tcp_listener = TcpListener::bind(addr).expect("failed to bind");
-        let tls_acceptor = TlsTcpAcceptor::mozilla_intermediate_v5()
-            .expect("failed to create builder")
-            .ca_file("test/root.crt")
-            .certificate_chain_file("test/server.crt")
-            .private_key_file("test/server.key")
-            .build()
-            .expect("failed to initialize tls acceptor");
+//     }
 
-        Listener::from((tcp_listener, tls_acceptor))
-    }
+//     fn create_connector() -> Connector {
+//         let tls_connector = TlsTcpConnector::builder()
+//             .expect("failed to create builder")
+//             .ca_file("test/root.crt")
+//             .certificate_chain_file("test/client.crt")
+//             .private_key_file("test/client.key")
+//             .build()
+//             .expect("failed to initialize tls connector");
 
-    #[test]
-    fn listener() {
-        let _ = create_listener("127.0.0.1:0");
-    }
+//         Connector::from(tls_connector)
+//     }
 
-    #[test]
-    fn connector() {
-        let _ = create_connector();
-    }
+//     fn create_listener(addr: &'static str) -> Listener {
+//         let tcp_listener = TcpListener::bind(addr).expect("failed to bind");
+//         let tls_acceptor = TlsTcpAcceptor::mozilla_intermediate_v5()
+//             .expect("failed to create builder")
+//             .ca_file("test/root.crt")
+//             .certificate_chain_file("test/server.crt")
+//             .private_key_file("test/server.key")
+//             .build()
+//             .expect("failed to initialize tls acceptor");
 
-    #[test]
-    fn ping_pong() {
-        let connector = create_connector();
-        let listener = create_listener("127.0.0.1:0");
+//         Listener::from((tcp_listener, tls_acceptor))
+//     }
 
-        let addr = listener.local_addr().expect("listener has no local addr");
+//     #[test]
+//     fn listener() {
+//         let _ = create_listener("127.0.0.1:0");
+//     }
 
-        let mut client_stream = connector.connect(addr).expect("failed to connect");
-        std::thread::sleep(std::time::Duration::from_millis(100));
-        let mut server_stream = listener.accept().expect("failed to accept");
+//     #[test]
+//     fn connector() {
+//         let _ = create_connector();
+//     }
 
-        let mut server_handshake_complete = false;
-        let mut client_handshake_complete = false;
+//     #[test]
+//     fn ping_pong() {
+//         let connector = create_connector();
+//         let listener = create_listener("127.0.0.1:0");
 
-        while !(server_handshake_complete && client_handshake_complete) {
-            if !server_handshake_complete {
-                std::thread::sleep(std::time::Duration::from_millis(100));
-                if server_stream.do_handshake().is_ok() {
-                    server_handshake_complete = true;
-                }
-            }
+//         let addr = listener.local_addr().expect("listener has no local addr");
 
-            if !client_handshake_complete {
-                std::thread::sleep(std::time::Duration::from_millis(100));
-                if client_stream.do_handshake().is_ok() {
-                    client_handshake_complete = true;
-                }
-            }
-        }
+//         let mut client_stream = connector.connect(addr).expect("failed to connect");
+//         std::thread::sleep(std::time::Duration::from_millis(100));
+//         let mut server_stream = listener.accept().expect("failed to accept");
 
-        std::thread::sleep(std::time::Duration::from_millis(100));
+//         let mut server_handshake_complete = false;
+//         let mut client_handshake_complete = false;
 
-        client_stream
-            .write_all(b"PING\r\n")
-            .expect("failed to write");
-        client_stream.flush().expect("failed to flush");
+//         while !(server_handshake_complete && client_handshake_complete) {
+//             if !server_handshake_complete {
+//                 std::thread::sleep(std::time::Duration::from_millis(100));
+//                 if server_stream.do_handshake().is_ok() {
+//                     server_handshake_complete = true;
+//                 }
+//             }
 
-        std::thread::sleep(std::time::Duration::from_millis(100));
+//             if !client_handshake_complete {
+//                 std::thread::sleep(std::time::Duration::from_millis(100));
+//                 if client_stream.do_handshake().is_ok() {
+//                     client_handshake_complete = true;
+//                 }
+//             }
+//         }
 
-        let mut buf = [0; 4096];
+//         std::thread::sleep(std::time::Duration::from_millis(100));
 
-        match server_stream.read(&mut buf) {
-            Ok(6) => {
-                assert_eq!(&buf[0..6], b"PING\r\n");
-                server_stream
-                    .write_all(b"PONG\r\n")
-                    .expect("failed to write");
-            }
-            Ok(n) => {
-                panic!("read: {} bytes but expected 6", n);
-            }
-            Err(e) => {
-                panic!("error reading: {}", e);
-            }
-        }
+//         client_stream
+//             .write_all(b"PING\r\n")
+//             .expect("failed to write");
+//         client_stream.flush().expect("failed to flush");
 
-        std::thread::sleep(std::time::Duration::from_millis(100));
+//         std::thread::sleep(std::time::Duration::from_millis(100));
 
-        match client_stream.read(&mut buf) {
-            Ok(6) => {
-                assert_eq!(&buf[0..6], b"PONG\r\n");
-            }
-            Ok(n) => {
-                panic!("read: {} bytes but expected 6", n);
-            }
-            Err(e) => {
-                panic!("error reading: {}", e);
-            }
-        }
-    }
-}
+//         let mut buf = [0; 4096];
+
+//         match server_stream.read(&mut buf) {
+//             Ok(6) => {
+//                 assert_eq!(&buf[0..6], b"PING\r\n");
+//                 server_stream
+//                     .write_all(b"PONG\r\n")
+//                     .expect("failed to write");
+//             }
+//             Ok(n) => {
+//                 panic!("read: {} bytes but expected 6", n);
+//             }
+//             Err(e) => {
+//                 panic!("error reading: {}", e);
+//             }
+//         }
+
+//         std::thread::sleep(std::time::Duration::from_millis(100));
+
+//         match client_stream.read(&mut buf) {
+//             Ok(6) => {
+//                 assert_eq!(&buf[0..6], b"PONG\r\n");
+//             }
+//             Ok(n) => {
+//                 panic!("read: {} bytes but expected 6", n);
+//             }
+//             Err(e) => {
+//                 panic!("error reading: {}", e);
+//             }
+//         }
+//     }
+// }
