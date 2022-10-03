6611cf42ef1ee1bf50fe097cafe9c2a7df550b55
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2022-10-03 09:35:22 -0700

    Write HTTP protocol

---
+++ src/protocol/http/src/error.rs
@@ -0,0 +1,62 @@
+// Copyright 2021 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+use crate::Response;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("unable to parse request")]
+    Unparseable(#[from] httparse::Error),
+    #[error("Content-Length header was invalid")]
+    BadContentLength,
+    #[error("Content-Length header was missing")]
+    MissingContentLength,
+    #[error("method was unsupported")]
+    BadRequestMethod,
+
+    /// Contains the number of additional bytes needed to parse the rest of the
+    /// request, if known.
+    #[error("not enough data present to parse the whole request")]
+    PartialRequest(Option<usize>),
+
+    #[error("an internal error occurred: {0}")]
+    InternalError(&'static str),
+}
+
+impl Error {
+    pub fn to_response(&self) -> Response {
+        match self {
+            Self::Unparseable(e) => Response::builder(400)
+                .should_close(true)
+                .header("Content-Type", b"text/plain")
+                .body(format!("Unable to parse request: {}", e).as_bytes()),
+            Self::BadRequestMethod => Response::builder(405)
+                .should_close(true)
+                .header("Content-Type", b"text/plain")
+                .body(
+                    format!("Unsupported method, only GET, PUT, and DELETE are supported")
+                        .as_bytes(),
+                ),
+            Self::BadContentLength => Response::builder(400)
+                .should_close(true)
+                .header("Content-Type", b"text/plain")
+                .body(format!("Content-Length header was invalid").as_bytes()),
+            Self::MissingContentLength => Response::builder(411)
+                .should_close(true)
+                .header("Content-Type", b"text/plain")
+                .body(
+                    format!("A Content-Length header is required for all PUT requests").as_bytes(),
+                ),
+            Self::InternalError(message) => Response::builder(500)
+                .should_close(true)
+                .header("Content-Type", b"text/plain")
+                .body(message.as_bytes()),
+
+            Self::PartialRequest(_) => Response::builder(500)
+                .should_close(true)
+                .header("Content-Type", b"text/plain")
+                .body(b"internal server error"),
+        }
+    }
+}
---
+++ src/protocol/http/src/lib.rs
@@ -0,0 +1,27 @@
+// Copyright 2021 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+//!
+
+#[macro_use]
+extern crate thiserror;
+
+mod error;
+pub mod request;
+pub mod response;
+mod util;
+
+pub use crate::error::Error;
+pub use crate::request::Headers;
+pub use crate::request::{Request, RequestData, RequestParser};
+pub use crate::response::Response;
+
+pub type Result<T> = std::result::Result<T, Error>;
+pub type ParseResult = Result<Request>;
+
+pub trait Storage {
+    fn get(&mut self, key: &[u8], headers: &Headers) -> Response;
+    fn put(&mut self, key: &[u8], value: &[u8], headers: &Headers) -> Response;
+    fn delete(&mut self, key: &[u8], headers: &Headers) -> Response;
+}
